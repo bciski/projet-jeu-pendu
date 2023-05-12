234375af90da1afbd794f0b991a487bf95eddcf0
bciski <133124377+bciski@users.noreply.github.com>
2023-05-12 17:35:41 +0200

    Create start du jeu avec bienvenue.py
    ajout du code de début de partie 
    possibilité d'ajouter un pseudo

---
+++ start du jeu avec bienvenue.py
@@ -0,0 +1,13 @@
+ print("bienvenue , c’est parti pour un pendue")
+play=int(input("Tape 1 si tu veux jouer ! \n "))
+if play == 1 :  
+    prénom=input("Quel est ton pseudo ?")
+    print("\n")
+    print("Salut", prénom)
+    import random
+    
+
+liste_mots=["salade", "poisson", "porc", "tête", "plage", "yeux",
+                "feutre","dollar","compte","vodka","billet","boyaux",
+                "laser","arme","jeux","ballon","cahier","tente","argent",
+                "rivage","physique",]
